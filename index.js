e157b5e6b5baf3da268f338b9eed993a1fd06c4f
Moshe <102148360+MosheHadadG@users.noreply.github.com>
2023-06-20 18:05:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+  /* options */
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let onlineUsers = [];
+
+const addNewUser = ({ userId, username, socketId }) => {
+  !onlineUsers.some((user) => user.userId === userId) &&
+    onlineUsers.push({ userId, username, socketId });
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUsers.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  console.log("someone has connected");
+
+  socket.on("newSocketUser", ({ userId, username }) => {
+    addNewUser({ userId, username, socketId: socket.id });
+    // console.log({ onlineUsers });
+  });
+
+  socket.on("sendNotification", ({ notification, type }) => {
+    if (
+      notification.NotificationsToRemovedParticipants &&
+      notification.NotificationsToRemovedParticipants.length > 0
+    ) {
+      notification.NotificationsToRemovedParticipants.map(
+        (notificationToRemovedParticipant) => {
+          const { receiver } = notificationToRemovedParticipant;
+          console.log(notificationToRemovedParticipant);
+          // console.log(receiver);
+          const receiverSocket = getUser(receiver);
+          if (!receiverSocket) return;
+          io.to(receiverSocket.socketId).emit("getNotification", {
+            notification: notificationToRemovedParticipant,
+            type,
+          });
+        }
+      );
+    } else {
+      const { receiver } = notification;
+
+      const receiverSocket = getUser(receiver);
+      // console.log({ receiverSocket });
+      if (!receiverSocket) return;
+      io.to(receiverSocket.socketId).emit("getNotification", {
+        notification,
+        type,
+      });
+    }
+  });
+
+  socket.on("sendRequestNotification", ({ requestNotification }) => {
+    const { receiver } = requestNotification;
+    const receiverSocket = getUser(receiver);
+    if (!receiverSocket) return;
+    io.to(receiverSocket.socketId).emit("getRequestNotification", {
+      requestNotification,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    console.log("someone has left", { onlineUsers });
+  });
+});
+
+io.listen(5005);
